a5e0175b7848fc93283b176ea567d20af3f6d0e2
Marija Voroncova <marijagrigule@gmail.com>
2024-07-10 19:51:46 +0300

    tested that homepage opens

--- src/test/specs/test.e2e.js
+++ src/test/specs/test.e2e.js
@@ -1,15 +1,10 @@
-import {expect} from '@wdio/globals'
-import LoginPage from '../pageobjects/login.page.js'
-import SecurePage from '../pageobjects/secure.page.js'
+import TrelloHomePage from '../pageobjects/pages/TrelloHome.page.js'
+
+const trelloHomepage = new TrelloHomePage()
 
 describe('My Login application', () => {
-  it('should login with valid credentials', async () => {
-    await LoginPage.open()
-
-    await LoginPage.login('tomsmith', 'SuperSecretPassword!')
-    await expect(SecurePage.flashAlert).toBeExisting()
-    await expect(SecurePage.flashAlert).toHaveTextContaining(
-        'You logged into a secure area!')
+  it('should open Trello hompage', async () => {
+    await trelloHomepage.open()
   })
 })
 
