fcb341532dd8e3890c0124c202ac51f1f3c636da
Marija Voroncova <marijagrigule@gmail.com>
2024-07-10 18:18:01 +0300

    initial commit. added .gitignore, set up wdio, configured eslint

---
+++ src/configs/eslint.config.js
@@ -0,0 +1,21 @@
+import globals from 'globals'
+import pluginJs from '@eslint/js'
+import google from 'eslint-config-google'
+
+
+export default [
+  {languageOptions: {globals: globals.node}},
+  pluginJs.configs.recommended,
+  {
+    ...google,
+    rules: {
+      ...google.rules,
+      'valid-jsdoc': 'off',
+      'require-jsdoc': 'off',
+      'default-case': 'error',
+      'semi': ['error', 'never'],
+      'func-call-spacing': ['error', 'never'],
+      'space-in-parens': ['error', 'never'],
+    },
+  },
+]
