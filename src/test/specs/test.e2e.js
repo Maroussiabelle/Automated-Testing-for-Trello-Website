d0b758e1fabc773e42087ddfde2552308b53ba40
Marija Voroncova <marijagrigule@gmail.com>
2024-07-10 20:53:22 +0300

    test for closing cookie banner and clicking log in button

--- src/test/specs/test.e2e.js
+++ src/test/specs/test.e2e.js
@@ -3,8 +3,12 @@ import TrelloHomePage from '../pageobjects/pages/TrelloHome.page.js'
 const trelloHomepage = new TrelloHomePage()
 
 describe('My Login application', () => {
-  it('should open Trello hompage', async () => {
+  it('should open Trello homepage', async () => {
     await trelloHomepage.open()
+    await trelloHomepage.cookieBanner.tryClose()
+    await trelloHomepage.header.item('loginButton').waitForDisplayed()
+    await trelloHomepage.header.item('loginButton').scrollIntoView()
+    await trelloHomepage.header.item('loginButton').click()
   })
 })
 
