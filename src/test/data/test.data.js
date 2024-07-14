47f511e1cce794e8e33903bc019f484138fe24ba
Marija Voroncova <marijagrigule@gmail.com>
2024-07-14 18:18:01 +0300

    added board title and board background to test data

--- src/test/data/test.data.js
+++ src/test/data/test.data.js
@@ -1,4 +1,6 @@
 export const TEST_DATA = {
   originalUsername: 'jstestswdio2',
   newUsername: 'jstestswdio2_updated',
+  boardTitle: 'New board',
+  backgroundMountainImageId: 'photo-1719935190609-c075f3dada9d',
 }
