36aee20a79564185b602418b11c4991859a811a6
Marija Voroncova <marijagrigule@gmail.com>
2024-07-17 16:06:18 +0300

    added a new selector that works for both "add a list" and "add another list" buttons

--- src/test/pageobjects/components/newBoard/board.component.js
+++ src/test/pageobjects/components/newBoard/board.component.js
@@ -3,8 +3,9 @@ import {$} from '@wdio/globals'
 
 
 const selectors = {
-  addAlistBtn:
-      'button.frrHNIWnTojsww.CSwccJ0PrMROzz.bxgKMAm3lq5BpA.SEj5vUdI3VvxDc',
+  addAlistBtn: 'button.frrHNIWnTojsww.CSwccJ0PrMROzz.bxgKMAm3lq5BpA.SEj5vUdI3VvxDc',
+  // addAlistBtn:
+  //  'button.frrHNIWnTojsww.CSwccJ0PrMROzz.bxgKMAm3lq5BpA.SEj5vUdI3VvxDc',
 
 }
 
