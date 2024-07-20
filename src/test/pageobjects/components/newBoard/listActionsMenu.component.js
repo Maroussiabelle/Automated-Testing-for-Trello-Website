b3f262359995a3c78c112d8086b7e52a4fcb4818
Marija Voroncova <marijagrigule@gmail.com>
2024-07-20 19:26:29 +0300

    added listActionsMenu component

---
+++ src/test/pageobjects/components/newBoard/listActionsMenu.component.js
@@ -0,0 +1,18 @@
+import {$} from '@wdio/globals'
+
+
+const selectors = {
+  archiveThisListBtn: './/button[@class="gJDsPins_eYkBM" and @data-testid="list-actions-archive-list-button"]',
+  archiveAllCardsInThisListBtn: '//span[@class="BmRHtH7FIX0jcL" and contains(text(), "Archive all cards in this list")]',
+}
+
+export default class ListActionsMenu {
+  get rootEl() {
+    return $('section.rX4pAv5sWHFNjp.js-react-root')
+  }
+
+
+  item(param) {
+    return this.rootEl.$(selectors[param])
+  }
+}
