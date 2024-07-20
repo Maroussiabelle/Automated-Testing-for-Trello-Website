38b1095aba329d1de38b08709b55b10865b56527
Marija Voroncova <marijagrigule@gmail.com>
2024-07-20 20:01:31 +0300

    6 test passing

--- src/test/specs/test.e2e.js
+++ src/test/specs/test.e2e.js
@@ -24,51 +24,51 @@ describe('Trello site functionality tests', () => {
     await dashboardPage.header.item('accountButton').waitForDisplayed()
   })
 
-  // it('should navigate to user boards page after login', async () => {
-  //   const currentUrl = await browser.getUrl()
-  //
-  //   expect(currentUrl).toBe('https://trello.com/u/jstestswdio2/boards')
-  // })
-  //
-  //
-  // it('should update the username in the user userProfile', async () => {
-  //   await dashboardPage.openProfileAndVisibilitySettings()
-  //   await profilePage.updateUsername(TEST_DATA.newUsername)
-  //   const updatedUsername = await profilePage.getUsername()
-  //
-  //   expect(updatedUsername).toEqual('@jstestswdio2_updated')
-  //   await profilePage.revertUsername(TEST_DATA.originalUsername)
-  // })
-  //
-  // it('should create a new board with the specified background and title', async () => {
-  //   await dashboardPage.open()
-  //   await dashboardPage.createBoard()
-  //   const displayedBoardTitle = await newBoardPage.boardHeader.item('displayedBoardTitle').getText()
-  //
-  //   expect(displayedBoardTitle).toEqual(TEST_DATA.boardTitle)
-  //   await newBoardPage.verifyBoardBackgroundCorrect()
-  //   await newBoardPage.deleteBoard()
-  // })
-  //
-  // it('should search for a @board with a specified title', async () => {
-  //   await dashboardPage.open()
-  //   await dashboardPage.header.item('searchFieldDiv').waitForDisplayed()
-  //   await dashboardPage.header.item('searchFieldDiv').click()
-  //   await dashboardPage.header.item('searchField').setValue(TEST_DATA.boardTitle)
-  //   await dashboardPage.searchDialogWrapper.item('searchResult').waitForDisplayed()
-  //   const foundBoardTitle = await dashboardPage.searchDialogWrapper.item('searchResult').getText()
-  //
-  //   expect(foundBoardTitle).toEqual(TEST_DATA.boardTitle)
-  // })
-  //
-  // it('should create a new list on an existing board', async () => {
-  //   await preConfiguredBoardPage.open()
-  //   await preConfiguredBoardPage.addList('New list')
-  //   const isListDisplayed = await preConfiguredBoardPage.newList.displayedListTitle('New list').waitForExist()
-  //
-  //   expect(isListDisplayed).toBe(true)
-  //   await preConfiguredBoardPage.deleteList()
-  // })
+  it('should navigate to user boards page after login', async () => {
+    const currentUrl = await browser.getUrl()
+
+    expect(currentUrl).toBe('https://trello.com/u/jstestswdio2/boards')
+  })
+
+
+  it('should update the username in the user userProfile', async () => {
+    await dashboardPage.openProfileAndVisibilitySettings()
+    await profilePage.updateUsername(TEST_DATA.newUsername)
+    const updatedUsername = await profilePage.getUsername()
+
+    expect(updatedUsername).toEqual('@jstestswdio2_updated')
+    await profilePage.revertUsername(TEST_DATA.originalUsername)
+  })
+
+  it('should create a new board with the specified background and title', async () => {
+    await dashboardPage.open()
+    await dashboardPage.createBoard()
+    const displayedBoardTitle = await newBoardPage.boardHeader.item('displayedBoardTitle').getText()
+
+    expect(displayedBoardTitle).toEqual(TEST_DATA.boardTitle)
+    await newBoardPage.verifyBoardBackgroundCorrect()
+    await newBoardPage.deleteBoard()
+  })
+
+  it('should search for a @board with a specified title', async () => {
+    await dashboardPage.open()
+    await dashboardPage.header.item('searchFieldDiv').waitForDisplayed()
+    await dashboardPage.header.item('searchFieldDiv').click()
+    await dashboardPage.header.item('searchField').setValue(TEST_DATA.boardTitle)
+    await dashboardPage.searchDialogWrapper.item('searchResult').waitForDisplayed()
+    const foundBoardTitle = await dashboardPage.searchDialogWrapper.item('searchResult').getText()
+
+    expect(foundBoardTitle).toEqual(TEST_DATA.boardTitle)
+  })
+
+  it('should create a new list on an existing board', async () => {
+    await preConfiguredBoardPage.open()
+    await preConfiguredBoardPage.addList('New list')
+    const isListDisplayed = await preConfiguredBoardPage.newList.displayedListTitle('New list').waitForExist()
+
+    expect(isListDisplayed).toBe(true)
+    await preConfiguredBoardPage.deleteList()
+  })
 
   it('should create a new card in a list on an existing board', async () => {
     await preConfiguredBoardPage.open()
