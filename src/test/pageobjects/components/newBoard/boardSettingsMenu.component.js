87dece4c8e6c92e979f9cc4a75405a950a8b9f22
Marija Voroncova <marijagrigule@gmail.com>
2024-07-26 12:24:20 +0300

    improved selectors

--- src/test/pageobjects/components/newBoard/boardSettingsMenu.component.js
+++ src/test/pageobjects/components/newBoard/boardSettingsMenu.component.js
@@ -3,9 +3,7 @@ import {$} from '@wdio/globals';
 const selectors = {
 
   closeBoardBtn: 'a.board-menu-navigation-item-link.board-menu-navigation-item-link-v2.js-close-board',
-  permanentlyDeleteBoardBtn:
-      'button.Bp80TGmc9hQIdE.WG_eo5n8x2vHvI.' +
-      'bxgKMAm3lq5BpA.V_9lMAQOdk_AYt.SEj5vUdI3VvxDc',
+  permanentlyDeleteBoardBtn: 'button[data-testid="close-board-delete-board-button"]',
 };
 
 export default class BoardSettingsMenu {
