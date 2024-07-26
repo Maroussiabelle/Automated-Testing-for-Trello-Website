cf98b5d807a890bf6f8fbbc52da5372e26ff9948
Marija Voroncova <marijagrigule@gmail.com>
2024-07-26 12:55:43 +0300

    improved selectors

--- src/test/pageobjects/components/preConfigured/board.component.js
+++ src/test/pageobjects/components/preConfigured/board.component.js
@@ -2,8 +2,7 @@ import {$} from '@wdio/globals';
 
 const selectors = {
   addAlistBtn:
-      'button.frrHNIWnTojsww.CSwccJ0PrMROzz.bxgKMAm3lq5BpA.SEj5vUdI3VvxDc',
-  boardHeader: 'h1.HKTtBLwDyErB_o',
+      'button[data-testid="list-composer-button"]',
 };
 
 export default class Board {
