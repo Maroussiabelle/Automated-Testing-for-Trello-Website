c2dce470d83bb343601d39ea006cf97c58a3b2c2
Marija Voroncova <marijagrigule@gmail.com>
2024-07-26 13:22:15 +0300

    improved selectors

--- src/test/pageobjects/components/preConfigured/cardComposer.component.js
+++ src/test/pageobjects/components/preConfigured/cardComposer.component.js
@@ -3,13 +3,13 @@ import {$} from '@wdio/globals';
 const selectors = {
   cardTitleInputField: 'textarea[data-testid="list-card-composer-textarea"]',
   addCardSubmitBtn: 'button[type="submit"]',
-  displayedCard: '//a[@class="NdQKKfeqJDDdX3" and contains(text(), "Test card")]',
+  displayedCard: '//a[@data-testid="card-name" and contains(text(), "Test card")]',
   addCardBtn: 'button[data-testid="list-card-composer-add-card-button"]',
 };
 
 export default class CardComposer {
   get rootEl() {
-    return $('//li[@data-testid="list-wrapper"]//h2[contains(text(), "List for cards") and @data-testid="list-name"]//ancestor::li[@class="bi0h3HALKXjfDq"]');
+    return $('//h2[contains(text(), "List for cards") and @data-testid="list-name"]//ancestor::li[@data-testid="list-wrapper"]');
   }
 
   item(param) {
