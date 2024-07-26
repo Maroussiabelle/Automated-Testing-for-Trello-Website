d7847bc788bb8e606786d4808bb8d8053ae57dee
Marija Voroncova <marijagrigule@gmail.com>
2024-07-26 13:42:01 +0300

    improved selectors

--- src/test/pageobjects/components/preConfigured/listActionsMenu.component.js
+++ src/test/pageobjects/components/preConfigured/listActionsMenu.component.js
@@ -1,13 +1,12 @@
 import {$} from '@wdio/globals';
 
 const selectors = {
-  archiveThisListBtn: './/button[@class="gJDsPins_eYkBM" and @data-testid="list-actions-archive-list-button"]',
-  archiveAllCardsInThisListBtn: '//span[@class="BmRHtH7FIX0jcL" and contains(text(), "Archive all cards in this list")]',
+  archiveThisListBtn: 'button[data-testid="list-actions-archive-list-button"]',
 };
 
 export default class ListActionsMenu {
   get rootEl() {
-    return $('section.rX4pAv5sWHFNjp.js-react-root');
+    return $('section[data-testid="list-actions-popover"]');
   }
 
   item(param) {
