8bbb039030baa707a5bf2d418bdbd1ccd6907d4f
Marija Voroncova <marijagrigule@gmail.com>
2024-07-26 11:07:34 +0300

    deleted unused logout page and component

--- src/test/pageobjects/pages/logOut.page.js
+++
@@ -1,8 +0,0 @@
-import LogoutConfirmationComponent from
-  '../components/logOut/logoutConfirmation.component.js';
-
-export default class LogOutPage {
-  constructor() {
-    this.logout = new LogoutConfirmationComponent();
-  }
-}
