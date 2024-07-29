4c77d64941357a2f8691f936045cad580275c50e
Marija Voroncova <marijagrigule@gmail.com>
2024-07-29 15:57:06 +0300

    added alias for chai expect, all tests passing

--- src/test/specs/test.e2e.js
+++ src/test/specs/test.e2e.js
@@ -7,10 +7,7 @@ import NewBoardPage from '../pageobjects/pages/newBoard.page.js';
 import {$, browser} from '@wdio/globals';
 import PreConfiguredBoardPage from '../pageobjects/pages/preConfiguredBoard.page.js';
 import WorkspaceSettingsPage from '../pageobjects/pages/workspaceSettings.page.js';
-import {assert} from 'chai';
-import {expect} from 'chai';
-import * as chai from 'chai';
-chai.should();
+import {assert, expect as chaiExpect} from 'chai';
 
 const trelloHomepage = new TrelloHomePage();
 const loginPage = new LoginPage();
@@ -39,81 +36,81 @@ describe('Trello site functionality tests', () => {
     }
   });
 
-  // it('should navigate to user boards page after login', async () => {
-  //   const currentUrl = await browser.getUrl();
-  //
-  //   // expect(currentUrl).toBe('https://trello.com/u/jstestswdio2/boards');
-  //   assert.equal(currentUrl, 'https://trello.com/u/jstestswdio2/boards', 'The current URL should match the expected URL');
-  // });
-  //
-  // it('should update the username in the user userProfile', async () => {
-  //   await dashboardPage.openProfileAndVisibilitySettings();
-  //   await profilePage.updateUsername(TEST_DATA.newUsername);
-  //   const elementsContainingAt = await profilePage.profileContent.rootEl.$$(`.//span[contains(text(), '@')]`);
-  //
-  //   expect(await elementsContainingAt.map((e) => e.getText())).contain(`@${TEST_DATA.newUsername}`);
-  //
-  //   await profilePage.revertUsername(TEST_DATA.originalUsername);
-  // });
-  //
-  // it('should create a new board with the specified background and title', async () => {
-  //   await dashboardPage.open();
-  //   const backgroundId = await dashboardPage.createBoardAndGetBackgroundId();
-  //   const displayedBoardTitle = await newBoardPage.boardHeader.item('displayedBoardTitle').getText();
-  //
-  //   expect(displayedBoardTitle).equal(TEST_DATA.boardTitle);
-  //   await newBoardPage.verifyBoardBackgroundCorrect(backgroundId);
-  //
-  //   await newBoardPage.deleteBoard();
-  // });
+  it('should navigate to user boards page after login', async () => {
+    const currentUrl = await browser.getUrl();
+
+    // expect(currentUrl).toBe('https://trello.com/u/jstestswdio2/boards');
+    assert.equal(currentUrl, 'https://trello.com/u/jstestswdio2/boards', 'The current URL should match the expected URL');
+  });
+
+  it('should update the username in the user userProfile', async () => {
+    await dashboardPage.openProfileAndVisibilitySettings();
+    await profilePage.updateUsername(TEST_DATA.newUsername);
+    const elementsContainingAt = await profilePage.profileContent.rootEl.$$(`.//span[contains(text(), '@')]`);
+
+    chaiExpect(await elementsContainingAt.map((e) => e.getText())).contain(`@${TEST_DATA.newUsername}`);
+
+    await profilePage.revertUsername(TEST_DATA.originalUsername);
+  });
+
+  it('should create a new board with the specified background and title', async () => {
+    await dashboardPage.open();
+    const backgroundId = await dashboardPage.createBoardAndGetBackgroundId();
+    const displayedBoardTitle = await newBoardPage.boardHeader.item('displayedBoardTitle').getText();
+
+    chaiExpect(displayedBoardTitle).equal(TEST_DATA.boardTitle);
+    await newBoardPage.verifyBoardBackgroundCorrect(backgroundId);
+
+    await newBoardPage.deleteBoard();
+  });
 
   it('should search for a board with a specified title', async () => {
     await dashboardPage.open();
     await dashboardPage.searchBoard();
     await dashboardPage.searchDialogWrapper.item('searchResult').waitForDisplayed();
     const isBoardFound = await dashboardPage.searchDialogWrapper.item('searchResult').isDisplayed();
-    isBoardFound.should.be.true;
-    // expect(isBoardFound).toBe(true);
+
+    expect(isBoardFound).toBe(true);
   });
 
-  // it('should create a new list on an existing board', async () => {
-  //   await preConfiguredBoardPage.open();
-  //   await preConfiguredBoardPage.addList('New list');
-  //   const isListDisplayed = await preConfiguredBoardPage.newList.displayedListTitle('New list').waitForExist();
-  //
-  //   expect(isListDisplayed).toBe(true);
-  //   await preConfiguredBoardPage.deleteList();
-  // });
-  //
-  // it('should create a new card in a list on an existing board', async () => {
-  //   await preConfiguredBoardPage.open();
-  //   await preConfiguredBoardPage.addCard();
-  //   const isCardDisplayed = await preConfiguredBoardPage.cardComposer.item('displayedCard').waitForExist();
-  //
-  //   expect(isCardDisplayed).toBe(true);
-  //   await preConfiguredBoardPage.deleteCard(TEST_DATA.cardTitle);
-  // });
-  //
-  // it('should filter cards on an existing board', async () => {
-  //   await preConfiguredBoardPage.filterCards();
-  //
-  //   expect(preConfiguredBoardPage.listForCards.card(TEST_DATA.cardForFilterTest)).toBeDisplayed();
-  // });
-  //
-  // it('should edit workspace details', async () => {
-  //   const editedWorkspaceName = 'Edited workspace name';
-  //   const testWorkspaceDescription = 'added description for test workspace';
-  //   const originalWorkspaceName = 'Trello Workspace';
-  //
-  //   await dashboardPage.open();
-  //   await dashboardPage.boardSectionHeader.item('settingsBtn').click();
-  //   await workspaceSettingsPage.updateWorkspaceNameAndDescription(editedWorkspaceName, testWorkspaceDescription);
-  //
-  //   const displayedWorkspaceTitle = await workspaceSettingsPage.workspaceSideMenu.item('displayedWorkspaceTitle').getText();
-  //   expect(displayedWorkspaceTitle).toEqual(editedWorkspaceName);
-  //
-  //   const displayedWorkspaceDescription = await workspaceSettingsPage.workspaceDetails.item('description').getText();
-  //   expect(displayedWorkspaceDescription).toEqual(testWorkspaceDescription);
-  //   await workspaceSettingsPage.revertChangeofWorkspaceNameAndDescription(originalWorkspaceName);
-  // });
+  it('should create a new list on an existing board', async () => {
+    await preConfiguredBoardPage.open();
+    await preConfiguredBoardPage.addList('New list');
+    const isListDisplayed = await preConfiguredBoardPage.newList.displayedListTitle('New list').waitForExist();
+
+    expect(isListDisplayed).toBe(true);
+    await preConfiguredBoardPage.deleteList();
+  });
+
+  it('should create a new card in a list on an existing board', async () => {
+    await preConfiguredBoardPage.open();
+    await preConfiguredBoardPage.addCard();
+    const isCardDisplayed = await preConfiguredBoardPage.cardComposer.item('displayedCard').waitForExist();
+
+    expect(isCardDisplayed).toBe(true);
+    await preConfiguredBoardPage.deleteCard(TEST_DATA.cardTitle);
+  });
+
+  it('should filter cards on an existing board', async () => {
+    await preConfiguredBoardPage.filterCards();
+
+    expect(preConfiguredBoardPage.listForCards.card(TEST_DATA.cardForFilterTest)).toBeDisplayed();
+  });
+
+  it('should edit workspace details', async () => {
+    const editedWorkspaceName = 'Edited workspace name';
+    const testWorkspaceDescription = 'added description for test workspace';
+    const originalWorkspaceName = 'Trello Workspace';
+
+    await dashboardPage.open();
+    await dashboardPage.boardSectionHeader.item('settingsBtn').click();
+    await workspaceSettingsPage.updateWorkspaceNameAndDescription(editedWorkspaceName, testWorkspaceDescription);
+
+    const displayedWorkspaceTitle = await workspaceSettingsPage.workspaceSideMenu.item('displayedWorkspaceTitle').getText();
+    expect(displayedWorkspaceTitle).toEqual(editedWorkspaceName);
+
+    const displayedWorkspaceDescription = await workspaceSettingsPage.workspaceDetails.item('description').getText();
+    expect(displayedWorkspaceDescription).toEqual(testWorkspaceDescription);
+    await workspaceSettingsPage.revertChangeofWorkspaceNameAndDescription(originalWorkspaceName);
+  });
 });
