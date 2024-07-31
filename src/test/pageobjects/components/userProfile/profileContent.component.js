a00ee9a0a1f2c8bd9ac239841bd1be9265c2e24e
Marija Voroncova <marijagrigule@gmail.com>
2024-07-31 14:08:34 +0300

    added method that returns selector of Username containing certain text

--- src/test/pageobjects/components/userProfile/profileContent.component.js
+++ src/test/pageobjects/components/userProfile/profileContent.component.js
@@ -1,17 +1,15 @@
 import {$} from '@wdio/globals';
-import {TEST_DATA} from '../../../data/test.data.js';
-
-const selectors = {
-  displayedUsername: `span*=${TEST_DATA.newUsername}`,
-
-};
 
 export default class ProfileContent {
+  constructor(userName) {
+    this.userName = userName;
+  }
+
   get rootEl() {
     return $('div#content');
   }
 
-  item(param) {
-    return this.rootEl.$(selectors[param]);
+  displayedUserName(userName) {
+    return this.rootEl.$(`span*=${userName}`);
   }
 }
