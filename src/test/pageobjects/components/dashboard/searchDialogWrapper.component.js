8c74b2371dbb27a580567d13c9a8bbce749a8d80
Marija Voroncova <marijagrigule@gmail.com>
2024-07-31 14:09:38 +0300

    added methods getting search result by text, removed test data from selector

--- src/test/pageobjects/components/dashboard/searchDialogWrapper.component.js
+++ src/test/pageobjects/components/dashboard/searchDialogWrapper.component.js
@@ -1,17 +1,21 @@
 import {$} from '@wdio/globals';
-import {TEST_DATA} from '../../../data/test.data.js';
-
-const selectors = {
-  searchResult: `//div[@data-testid="trello-hover-preview-popper-container"]//span[text()="${TEST_DATA.boardForSearch}"]`,
-
-};
 
 export default class SearchDialogWrapper {
   get rootEl() {
     return $('div[data-test-id="search-dialog-dialog-wrapper"]');
   }
 
-  item(param) {
-    return this.rootEl.$(selectors[param]);
+  getSearchResultByText(text) {
+    return this.rootEl.$(`//div[@data-testid="trello-hover-preview-popper-container"]//span[text()="${text}"]`);
+  }
+
+  async waitForSearchResult(text, timeout) {
+    const element = this.getSearchResultByText(text);
+    await element.waitForDisplayed({timeout});
+  }
+
+  async isSearchResultDisplayed(text) {
+    const element = this.getSearchResultByText(text);
+    return await element.isDisplayed();
   }
 }
