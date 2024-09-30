ddfb1630293f478cb22e47f44e597336fef42f21
Marija Voroncova <marijagrigule@gmail.com>
2024-09-30 11:03:00 +0300

    fixed selector for a "close board" button, commented out "headless mode" to enable debugging

--- wdio/test/pageobjects/components/newBoard/boardSettingsMenu.component.js
+++ wdio/test/pageobjects/components/newBoard/boardSettingsMenu.component.js
@@ -1,7 +1,7 @@
 import BaseComponent from '../common/base.component.js';
 
 const selectors = {
-  closeBoardBtn: 'a.board-menu-navigation-item-link.board-menu-navigation-item-link-v2.js-close-board',
+  closeBoardBtn: '//button[contains(@class, "js-close-board")]',
   permanentlyDeleteBoardBtn: 'button[data-testid="close-board-delete-board-button"]',
 };
 
